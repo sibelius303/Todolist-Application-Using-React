d3dca2e6a2a1a46ebe2224ade2ce0043ab296ed5
sibelius303 <cesarev17@gmail.com>
2022-09-17 00:25:46 +0000

    Falta terminar logica y estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,54 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setImputValue] = useState('');
+	const [inputPrioridad, setImputPrioridad] = useState ('');
+	const [ todos , setTodos ] = useState([])
+
+	function handleChange(e){
+		const value= e.target.value
+		setImputValue(value);
+	}
+
+	function changePrioridad(e){
+		const prioridadI = e.target.value;
+		setImputPrioridad(prioridadI);
+
+	}
+
+	
+	function handleAdd(e){
+		let copia = [...todos];
+		copia.push(inputValue);
+		copia.push(inputPrioridad);
+		setTodos(copia);
+		setImputValue('');
+		setImputPrioridad('');
+
+	}
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="todoImput">
+				<input type="text" placeholder="Añadir Tarea" value={inputValue} onChange={handleChange}/>
+				<input type="text" placeholder="Prioridad de la Tarea" value={inputPrioridad} onChange={changePrioridad}/>
+				<button onClick={handleAdd}>Crear Tarea</button>
+			</div>
+			<div className="todoContainer">
+				{
+					todos.map((todo,index)=> {
+						return (
+							<div className="newTodo" key={index}>
+								{todo}
+							</div>
+						) 
+					})
+				}	
+			</div>
+
 		</div>
+
 	);
 };
 
