5c4213a0d367bb6875e9b099c87044440bb5424b
sibelius303 <cesarev17@gmail.com>
2022-09-20 00:59:45 +0000

    Falta boton de delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,37 +7,44 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [inputValue, setImputValue] = useState('');
-	const [inputPrioridad, setImputPrioridad] = useState ('');
-	const [ todos , setTodos ] = useState([])
+	const [ todos , setTodos ] = useState([]);
 
 	function handleChange(e){
 		const value= e.target.value
 		setImputValue(value);
 	}
 
-	function changePrioridad(e){
-		const prioridadI = e.target.value;
-		setImputPrioridad(prioridadI);
-
-	}
-
-	
+		
 	function handleAdd(e){
 		let copia = [...todos];
 		copia.push(inputValue);
-		copia.push(inputPrioridad);
 		setTodos(copia);
 		setImputValue('');
-		setImputPrioridad('');
-
 	}
 
+	function handleKeyDown(e){
+		
+		if (e.key === 'Enter') {
+			if(inputValue === ""){
+				alert('Escribe una tarea');
+				return
+			}
+			let copia = [...todos];
+			copia.push(inputValue);
+			setTodos(copia);
+			setImputValue('');
+		} 
+	}
 
+	// function handleDelete(todo,index){
+	// 	let temp = todos.filter(todo, index => index != index);
+	// 	setTodos(temp);
+	// }
+	
 	return (
 		<div className="text-center">
 			<div className="todoImput">
-				<input type="text" placeholder="Añadir Tarea" value={inputValue} onChange={handleChange}/>
-				<input type="text" placeholder="Prioridad de la Tarea" value={inputPrioridad} onChange={changePrioridad}/>
+				<input type="text" placeholder="Añadir Tarea" value={inputValue} onChange={handleChange} onKeyDown={handleKeyDown}/>
 				<button onClick={handleAdd}>Crear Tarea</button>
 			</div>
 			<div className="todoContainer">
@@ -44,7 +52,7 @@ const Home = () => {
 					todos.map((todo,index)=> {
 						return (
 							<div className="newTodo" key={index}>
-								{todo}
+								{todo}<button>X</button>
 							</div>
 						) 
 					})
